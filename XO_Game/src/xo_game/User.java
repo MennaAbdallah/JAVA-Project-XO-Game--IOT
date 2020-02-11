f7417a85cfa2efe7537167c2aa88e507ba00e283
Abdelrahman Mashael <abdomashael@yahoo.com>
2020-02-11 16:54:25 +0200

    add user class

---
+++ XO_Game/src/xo_game/User.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xo_game;
+
+/**
+ *
+ * @author mashael
+ */
+public class User {
+    
+    private String userName,nickName,password,email;
+    private int _id,score,status;
+
+    public User() {
+    }
+
+    public User(String userName, String nickName, String password, String email, int _id, int score, int status) {
+        this.userName = userName;
+        this.nickName = nickName;
+        this.password = password;
+        this.email = email;
+        this._id = _id;
+        this.score = score;
+        this.status = status;
+    }
+    
+    
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int _id) {
+        this._id = _id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userName=" + userName + ", nickName=" + nickName + ", password=" + password + ", email=" + email + ", _id=" + _id + ", score=" + score + ", status=" + status + '}';
+    }
+    
+    
+    
+}
