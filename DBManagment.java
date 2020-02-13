505ca69c7b4a2617de5ff2efad17b51b38d9ccd1
NesreenKhalid <38976296+NesreenKhalid@users.noreply.github.com>
2020-02-13 13:42:28 +0200

    DB connect method added

--- DBManagment.java
+++ DBManagment.java
@@ -20,42 +20,71 @@ import java.util.logging.Logger;
  */
 public class DBManagment {
 
-    private Connection c = null;
-    private String dbPort;
-    private String dbDriver;
-    private String dbUser ;
-    private String dbPass ;
+    private static Connection c = null;
+    private static String dbPort = "3306";
+    private static String dbDriver = "com.mysql.jdbc.Driver";
+    private static String dbUser = "root";
+    private static String dbPass ="";
 
     //////////// for testing only
     public static void main(String[] args) {
         try {
             DBManagment dbm = new DBManagment();
+            DBManagment.connect();
             User user = new User("nesr","nesr","nesr","duur",3,10,1);
-             boolean s = dbm.checkPassword(3,"nesr");
+             boolean s = dbm.checkPassword(3,"neesr");
              System.out.println(s);
-        } catch (SQLException ex) {
-            Logger.getLogger(DBManagment.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
+        } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(DBManagment.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
     ///////////////////////////
-    
-    public DBManagment() throws SQLException, ClassNotFoundException {
+    public DBManagment(){
+    }
+
+    public static boolean connect() throws ClassNotFoundException, SQLException {
         try {
-            //Class.forName("org.sqlite.JDBC");
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            //c = DriverManager.getConnection("jdbc:sqlite:USER1");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaGame", "root", "");
-            System.out.println("Opened database successfully");
-        } catch (InstantiationException ex) {
-            Logger.getLogger(DBManagment.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
+            Class.forName(dbDriver).newInstance();
+            c = DriverManager.getConnection("jdbc:mysql://localhost:"+dbPort+"/javaGame", dbUser, dbPass);
+            return true;
+        } catch (InstantiationException | IllegalAccessException ex) {
             Logger.getLogger(DBManagment.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        return false;
     }
+
+    public static void setPort(String dbPort) {
+        DBManagment.dbPort = dbPort;
+    }
+
+    public static void setDriver(String dbDriver) {
+        DBManagment.dbDriver = dbDriver;
+    }
+
+    public static void setDBUser(String dbUser) {
+        DBManagment.dbUser = dbUser;
+    }
+
+    public static void setDBPassword(String dbPass) {
+        DBManagment.dbPass = dbPass;
+    }
+
+    public static String getPort() {
+        return dbPort;
+    }
+
+    public static String getDriver() {
+        return dbDriver;
+    }
+
+    public static String getDBUser() {
+        return dbUser;
+    }
+
+    public static String getDBPass() {
+        return dbPass;
+    }
+    
 
     public int getScore(int id){
         try {
