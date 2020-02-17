02d1bb65396e93a26de0d058c8732816636976d4
fadyjan <fady.jan95@gmail.com>
2020-02-17 23:56:07 +0200

    Adding Music To the Game

--- XOGui/src/LoginScene/Main.java
+++ XOGui/src/LoginScene/Main.java
@@ -1,19 +1,27 @@
 package LoginScene;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+    
+        String path ="C:\\Users\\fegoo\\Desktop\\JAVA-Project-XO-Game--IOT\\XOGui\\src\\Song1.mp3";
+    Media media = new Media (new File (path).toURI().toString());
+    MediaPlayer mediaplayer = new MediaPlayer(media);
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene(root, 909, 509);
+         mediaplayer.play();
         primaryStage.setTitle("TicTacToe");
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
